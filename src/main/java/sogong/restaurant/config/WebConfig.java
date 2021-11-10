a343e8dc49a9ecef0edcc9f1ff33b70c3e85014c
Cho Hyun Jun <chohyunjun1111@gmail.com>
2021-11-10 14:23:24 +0900

    import HttpMethod

--- src/main/java/sogong/restaurant/config/WebConfig.java
+++ src/main/java/sogong/restaurant/config/WebConfig.java
@@ -3,6 +3,7 @@ package sogong.restaurant.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
