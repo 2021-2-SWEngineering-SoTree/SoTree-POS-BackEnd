2f1057ef8c3b111823bc69efb236f239bdccdcb6
tommy1610 <75344562+tommy16102@users.noreply.github.com>
2021-11-10 13:10:22 +0900

    Update WebConfig.java

--- src/main/java/sogong/restaurant/config/WebConfig.java
+++ src/main/java/sogong/restaurant/config/WebConfig.java
@@ -8,6 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowCredentials(true).allowedOrigins("http://localhost:3000");
+        registry.addMapping("/**").allowCredentials(true).allowedOrigins("http://localhost:3000")
+              .allowedMethods(HttpMethod.HEAD.name(), HttpMethod.POST.name(), HttpMethod.GET.name(), HttpMethod.PUT.name(), HttpMethod.DELETE.name());
+
     }
 }
