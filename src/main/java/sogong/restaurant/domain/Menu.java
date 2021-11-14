989a0e95729792f601d608d33dc6edb6071319c3
Cho Hyun Jun <chohyunjun1111@gmail.com>
2021-11-14 19:12:13 +0900

    orderdetailList -> Menu 단방향

--- src/main/java/sogong/restaurant/domain/Menu.java
+++ src/main/java/sogong/restaurant/domain/Menu.java
@@ -7,8 +7,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Setter
@@ -31,8 +29,9 @@ public class Menu {
     @JoinColumn(name = "BranchId")
     private Manager manager;
 
-    @OneToMany(mappedBy = "menu")
-    private List<OrderDetail> orderDetailList = new ArrayList<>();
+    // orderdetailList -> Menu 단방향
+//    @OneToMany(mappedBy = "menu")
+//    private List<OrderDetail> orderDetailList = new ArrayList<>();
 
 
 }
