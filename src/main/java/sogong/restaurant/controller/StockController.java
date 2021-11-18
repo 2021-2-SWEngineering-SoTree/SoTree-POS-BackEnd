5ee676645fd8f120bbdddfc338f8f7442d4ef7a8
HojunLee <cajun7@naver.com>
2021-11-19 04:14:59 +0900

    stock add change

--- src/main/java/sogong/restaurant/controller/StockController.java
+++ src/main/java/sogong/restaurant/controller/StockController.java
@@ -46,11 +46,13 @@ public class StockController {
         stock.setManager(manager);
         stockService.saveStock(stock);
 
-        System.out.println(stockvo.getEmployeeId());
-
 
         for (StockDetail stockDetail : stockvo.getStockDetailList()) {
-            Employee employee = stockDetail.getEmployee();
+            System.out.println(stockDetail);
+            //Employee employee = stockDetail.getEmployee();
+            Employee employee = employeeRepository.findById(stockvo.getEmployeeId())
+                    .orElseThrow(() -> new NoSuchElementException("해당 직원이 없습니다."));
+            System.out.println("직원 : "+ employee.getId());
             stockDetail.setStock(stock);
             stockDetail.setEmployee(employee);
             stockDetailService.addStockDetail(stockDetail);
