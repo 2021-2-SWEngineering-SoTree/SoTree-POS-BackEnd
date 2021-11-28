f3945b115cfc33bc5868d88a820c40ee4cdc93ad
Cho Hyun Jun <chohyunjun1111@gmail.com>
2021-11-29 01:58:26 +0900

    Event 수정

--- src/main/java/sogong/restaurant/domain/Event.java
+++ src/main/java/sogong/restaurant/domain/Event.java
@@ -15,9 +15,9 @@ import javax.persistence.*;
 @NoArgsConstructor
 public class Event {
 
-    @javax.persistence.Id
+    @Id
     @Column(name = "EventId")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.TABLE)
     private Long id;
 
     private String eventName;
@@ -26,7 +26,7 @@ public class Event {
     private Integer eventDiscountValue; // 할인 가격 (정수)
     @ColumnDefault(value = "-1")
     private Double eventDiscountRate; // 할인 비율 (0~1 사이)
-    
+
     private int criticalPoint; // 이벤트 시작 임계점
 
     @ManyToOne
